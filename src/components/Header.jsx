6567ff47d05c674dbaddeb5d15d775c3f2956965
Victor Falconi <v.falconi.m@gmail.com>
2024-10-24 19:06:05 -0500

    Header init

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import headerStyles from '../styles/Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={headerStyles.container}>
+        Logo
+    </div>
+  )
+}
+
+export default Header;
