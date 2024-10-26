4fb86cfa821aed8388e0076fed4ed6cf7cb00243
AndreaaGM <moon.lp.13@gmail.com>
2024-10-26 16:16:22 -0600

    Detail init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import Header from "./components/Header.jsx";
-
+import Detail from "./Routes/Detail.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-      <Header/>
-    </>
-  )
+    <Router>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/detail/:id" element={<Detail />} />
+          {/* Otras rutas */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
