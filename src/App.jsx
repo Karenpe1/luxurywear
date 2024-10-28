4bf9b62ecdc071300e54e6e1379174b742fb737f
Victor Falconi <v.falconi.m@gmail.com>
2024-10-27 21:33:36 -0500

    Resolver conflictos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import Body from "./components/Body.jsx";
 import Header from "./components/Header.jsx";
-import TopRentas from "./components/TopRentas.jsx";
 import Detail from "./Routes/Detail.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Routes/Home.jsx";
 
 function App() {
   return (
@@ -11,12 +10,11 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/detail/:id" element={<Detail />} />
-          <Route path="/">
-              <Body/>
-              <TopRentas/>
-          </Route>
+          <Route path="/" element={<Home/>}/>
         </Routes>
       </div>
     </Router>
   );
 }
+
+export default App;
