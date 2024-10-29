f19c0c2bf196305639898f689b90e9a4c5c49ff1
tineojf <92743762+tineodev@users.noreply.github.com>
2024-10-29 16:53:57 -0500

    fix - reemplazar archivo toprentas

--- src/components/TopRentas.jsx
+++ src/components/TopRentas.jsx
@@ -1,77 +1,37 @@
-import React, { useEffect, useState } from "react";
-import Button from "./Button";
+import { useEffect, useState } from "react";
 import RentaStyle from "../styles/topRentas.module.css";
-
-const listaProductos = [
-  {
-    id: 1,
-    url: "/img/coctel1.jpg",
-    descripcion: "vestido de noche",
-  },
-  {
-    id: 2,
-    url: "/img/coctelDos2.png",
-    descripcion: "vestido de noche dorado",
-  },
-  {
-    id: 3,
-    url:"/img/coctelTres4.jpg",
-    descripcion: "vestido de noche con volados",
-  },
-  {
-    id: 4,
-    url: "/img/madrina1.jpg",
-    descripcion: "vestido de madrina azul",
-  },
-  {
-    id: 5,
-    url: "/img/novia3.jpg",
-    descripcion: "vestido de novia estilo princesa",
-  },
-  {
-    id: 6,
-    url: "/img/quince1.jpg",
-    descripcion: "vestido quinceañera rosado",
-  },
-];
-const aleatorizar = (array) => {
-  //se crea una copia del array original
-  let arrayAleatorio = [...array];
-  //Iterar desde el Final del Array hasta el Principio:
-  for (let i = arrayAleatorio.length - 1; i > 0; i--) {
-    //Generamos un Índice Aleatorio
-    const j = Math.floor(Math.random() * (i + 1));
-    //Intercambiamos los Elementos para aleatorizar el array segun el numero de 0 a i que obtuvimos en el indice j con el math.random
-    [arrayAleatorio[i], arrayAleatorio[j]] = [
-      arrayAleatorio[j],
-      arrayAleatorio[i],
-    ];
-  }
-  return arrayAleatorio;
-};
+import { Link } from "react-router-dom";
 
 const TopRentas = () => {
-  const [aleatorizarProductos, setAleatorizarProductos] = useState([]);
+  const [topRentas, setTopRentas] = useState([]);
+  const url = "http://localhost:8080/api/v1/products/top-rents";
 
   useEffect(() => {
-    // Mezclar los productos cada vez que se monta el componente
-    setAleatorizarProductos(aleatorizar(listaProductos));
+    const fetchData = async () => {
+      let respuesta = await fetch(url);
+      let data = await respuesta.json();
+      setTopRentas(data);
+      console.log("product", data);
+    };
+    fetchData();
   }, []);
+
   return (
-    <div className={RentaStyle.contenedor} >
+    <div className={RentaStyle.contenedor}>
       <h2 className={RentaStyle.titulo}>Top en Rentas</h2>
       <div className={RentaStyle.listaProductos}>
         {/*recorre lista aleatorizada */}
-        {aleatorizarProductos.map((data) => (
-          <div key={data.id} className={RentaStyle.producto}>
-            {/* Añadiend el botón debajo de la Card */}
-            <img className={RentaStyle.card} src={data.url} alt={data.descripcion} />
+        {topRentas.map((data) => (
+          <div key={data.productId} className={RentaStyle.producto}>
+            <img
+              className={RentaStyle.card}
+              src={data.images[0].url}
+              alt={data.name}
+            />
             {/* Añadiendo el botón debajo de la Card */}
-            <button
-             className={RentaStyle.detalle}
-            >
-              Ver detalle
-            </button>
+            <Link to={"/detail/" + data.productId}>
+              <button className={RentaStyle.detalle}>Ver detalle</button>
+            </Link>
           </div>
         ))}
       </div>
