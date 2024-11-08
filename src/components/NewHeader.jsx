0502bf6ec300fdbbf405348c114999354647b64e
karenpe1 <ka.gi.perez2000@gmail.com>
2024-11-08 11:33:31 -0500

    solucion boton inicio y registro

--- src/components/NewHeader.jsx
+++ src/components/NewHeader.jsx
@@ -1,35 +1,45 @@
 import styles from "../styles/NewHeader.module.css";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const NewHeader = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
-
-    const handleHamburger = () => {
-        setIsOpen(!isOpen);
-    }
+  const handleHamburger = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div className={styles.container}>
-        {isOpen && <div className={styles.menu}>
-            <div className={styles.menuHeader}>
-                <img className={styles.logo} src="logo.png"/>
-                <img src="cerrar.png" onClick={handleHamburger}/>
-            </div>
-        </div>}
-        <img className={styles.logo} src="logo.png"/>
-        <div className={styles.links}>
-            <span>Nosotros</span>
-            <span>Sostenibilidad</span>
-            <span>Contacto</span>
+      {isOpen && (
+        <div className={styles.menu}>
+          <div className={styles.menuHeader}>
+            <img className={styles.logo} src="logo.png" />
+            <img src="cerrar.png" onClick={handleHamburger} />
+          </div>
         </div>
-        <div className={styles.buttons}>
-            <button className={styles.button}>Iniciar Sesión</button>
-            <button className={styles.button}>Crear Cuenta</button>
-        </div>
-        <img className={styles.hamburger} src="menu.png" onClick={handleHamburger}/>
+      )}
+      <img className={styles.logo} src="logo.png" />
+      <div className={styles.links}>
+        <span>Nosotros</span>
+        <span>Sostenibilidad</span>
+        <span>Contacto</span>
+      </div>
+      <div className={styles.buttons}>
+        <Link to="/login">
+          <button className={styles.button}>Iniciar Sesión</button>
+        </Link>
+        <Link to="/register">
+          <button className={styles.button}>Crear Cuenta</button>
+        </Link>
+      </div>
+      <img
+        className={styles.hamburger}
+        src="menu.png"
+        onClick={handleHamburger}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default NewHeader;
