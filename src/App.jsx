0b50d1019d71a6776196464668b16952fddd7596
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-11-08 20:04:00 -0500

    feat: implement PrivateRoute and secure `/admin` route
    - Created `PrivateRoute` component in `src/components/PrivateRoute.jsx` to restrict access to specific routes based on user authentication and roles.
      - Redirects unauthenticated users to `/login`.
      - Redirects users without the required role to the home page (`/`).
      - Accepts `requiredRole` prop to enforce role-based access.
    
    - Updated `App.jsx` to:
      - Wrap the application with `AuthProvider` to provide authentication context.
      - Protect `/admin` route using `PrivateRoute`, ensuring only authenticated users with the `ADMIN` role can access it.
    
    This update enhances route security by implementing role-based and authentication-based access control.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-// src/App.jsx
 import NewHeader from "./components/NewHeader.jsx";
-import Footer from "./components/Footer.jsx"; // Importa el Footer
+import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 import Detail from "./Routes/Detail.jsx";
 import {
   Routes,
@@ -13,31 +13,40 @@ import Login from "./Routes/Login"
 import Admin from "./Routes/Admin.jsx";
 import Register from "./Routes/Register.jsx";
 import Search from "./components/Search.jsx";
+import { AuthProvider } from "./context/AuthContext";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
 
   const location = useLocation();
   const isLocation = location.pathname === "/register";
-  return (
-    <>
 
-    {!isLocation &&  ( 
+  return (
+    <AuthProvider>
+    {!isLocation && (
       <>
       <NewHeader />
+      {/* <Header /> */}
       <Search/>
       </>  )/*condicion para no mostrar el header en el registro */} 
         <div className={appStyles.container}>
           <Routes>
             <Route path="/detail/:id" element={<Detail />} />
             <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} /> {/* Ruta de inicio de sesión */}
-            <Route path="/admin" element={<Admin />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/admin"
+              element={
+                <PrivateRoute requiredRole="ADMIN">
+                  <Admin />
+                </PrivateRoute>
+              }
+            />
             <Route path="/register" element={<Register />} />
           </Routes>
         </div>
       {!isLocation && <Footer /> }
-
-    </>
+    </AuthProvider>
   );
 }
 
