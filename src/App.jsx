3e973f0a4e478ce5a0a85376cdd0a5c7ed565204
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-11-11 23:37:49 -0500

    feat: enhance App component with dynamic category routing and improved structure
    - Added dynamic route (`/:categoryName`) to render `PaginatedProductList` for category-specific pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,50 @@
 import NewHeader from "./components/NewHeader.jsx";
 import Detail from "./Routes/Detail.jsx";
-import {
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./Routes/Home.jsx";
 import appStyles from "./styles/App.module.css";
 import Login from "./Routes/Login";
+import Admin from "./Routes/Admin.jsx";
 import Register from "./Routes/Register.jsx";
 import Search from "./components/Search.jsx";
 import { AuthProvider } from "./context/AuthContext";
 import PrivateRoute from "./components/PrivateRoute";
 import NewFooter from "./components/NewFooter.jsx";
-import NewAdmin from "./Routes/NewAdmin.jsx";
+import PaginatedProductList from "./components/PaginatedProductList";
 
 function App() {
-
   const location = useLocation();
   const isLocation = location.pathname === "/register";
-  const currentPath = location.pathname;
 
   return (
     <AuthProvider>
     {!isLocation && (
       <>
       <NewHeader />
-      {/* <Header /> */}
-      {currentPath == "/" && <Search/>}
+      <Search />
       </>  )/*condicion para no mostrar el header en el registro */} 
         <div className={appStyles.container}>
           <Routes>
             <Route path="/detail/:id" element={<Detail />} />
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route
               path="/admin"
               element={
                 <PrivateRoute requiredRole="ADMIN">
-                  <NewAdmin />
+                  <Admin />
                 </PrivateRoute>
               }
             />
-            <Route path="/register" element={<Register />} />
+            {/* Dynamic category route */}
+            <Route
+              path="/:categoryName"
+              element={<PaginatedProductList />}
+            />
           </Routes>
         </div>
-      {!isLocation && <NewFooter /> }
+      {!isLocation && <NewFooter />}
     </AuthProvider>
   );
 }
