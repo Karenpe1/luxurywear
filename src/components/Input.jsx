f0428ea2b81f1bbada1599a01f86d8554b282cc9
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-11-12 19:25:07 -0500

    feat: add name prop support to Input component
    - Enhanced the `Input` component to accept a `name` prop for improved form handling and integration.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,13 @@
-import React from "react";
 import StyleInputs from "../styles/inputs.module.css";
 
-const Input = ({label, placeholder, type,value,onChange,error}) => {
+const Input = ({ label, placeholder, type, name, value, onChange, error }) => {
   return (
     <>
       <label className={StyleInputs.label}>{label}</label>
       <input
         className={StyleInputs.input}
         placeholder={placeholder}
+        name={name}
         type={type}
         value={value}
         onChange={onChange}
