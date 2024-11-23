0dd9228a207f8fad4a49159f62bf0fcc3bf40cfa
karenpe1 <ka.gi.perez2000@gmail.com>
2024-11-23 18:04:46 -0500

    solucionando problemas de cerrado del form en los inuts desplegables

--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -1,38 +1,56 @@
 import React, { useState } from "react";
 import StyleFile from "../styles/filePicker.module.css";
 
-const FilePicker = ({ label, type, accept, onChange, error, archivos}) => {
-    const [fileError, setFileError] =useState("")
-    const handleFileChange=(e)=>{
-        const files= e.target.files;
-        if(archivos === 1){
-            if (files.length < 1) {
-                setFileError("Por favor selecciona al menos un archivo.");
-              } else if (files.length > 1) {
-                setFileError("No puedes seleccionar más de un archivo.");
-              } else {
-                setFileError("");
-                onChange(e); // Pasa los archivos si están en el rango permitido
-              }
-        }else{
-            if (files.length < 1) {
-                setFileError("Por favor selecciona al menos un archivo.");
-              } else if (files.length > 5) {
-                setFileError("No puedes seleccionar más de cinco archivos.");
-              } else {
-                setFileError("");
-                onChange(e); // Pasa los archivos si están en el rango permitido
-              }
-        }
+const FilePicker = ({ label, type, accept, onChange, error, archivos }) => {
+  const [fileError, setFileError] = useState("");
+  const [fileName, setFileName] = useState(
+    "No se ha seleccionado ningun archivo "
+  );
+
+  const handleFileChange = (e) => {
+    const files = e.target.files;
+    if (archivos === 1) {
+      if (files.length < 1) {
+        setFileError("Por favor selecciona al menos un archivo.");
+      } else if (files.length > 1) {
+        setFileError("No puedes seleccionar más de un archivo.");
+      } else {
+        setFileError("");
+        setFileName(files[0].name); //nombre de archivo seleccionado
+        onChange(e); // Pasa los archivos si están en el rango permitido
+      }
+    } else {
+      if (files.length < 1) {
+        setFileError("Por favor selecciona al menos un archivo.");
+      } else if (files.length > 5) {
+        setFileError("No puedes seleccionar más de cinco archivos.");
+      } else {
+        setFileName(`${files.length} archivo(s) seleccionado(s)`); //nombre de archivo seleccionado
+        setFileError("");
+        onChange(e); // Pasa los archivos si están en el rango permitido
+      }
     }
+  };
   return (
-    <>
+    <div className={StyleFile.filePickerContainer}>
       <label className={StyleFile.labelFile}>{label}</label>
-      <input className={StyleFile.archivo} type={type} multiple accept={accept} onChange={handleFileChange} />
+      <div className={StyleFile.filePikerWrapper}>
+        <label className={StyleFile.customButton}>
+          Cargar Imágenes
+          <input
+            className={StyleFile.hiddenInput}
+            type={type}
+            multiple
+            accept={accept}
+            onChange={handleFileChange}
+          />
+        </label>
+        <span className={StyleFile.fileName}>{fileName}</span>
+      </div>
       {(fileError || error) && (
-        <span className={StyleFile.error}>{fileError || error }</span>
+        <div className={StyleFile.error}>{fileError || error}</div>
       )}
-    </>
+    </div>
   );
 };
 
