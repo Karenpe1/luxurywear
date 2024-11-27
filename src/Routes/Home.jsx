e7523be8e804085c4bf7b2f6894d737c440bc0e1
karenpe1 <ka.gi.perez2000@gmail.com>
2024-11-26 20:03:23 -0500

    creacion de pagina de reserva

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,9 +1,11 @@
 import Body from "../components/Body";
+import Checkout from "./Checkout";
 
 const Home = ({isSearch}) => {
   return (
     <>
       <Body isSearch={isSearch}/>
+      <Checkout/>
     </>
   );
 };
