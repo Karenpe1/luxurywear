b18a5842bb3afbdd190cfde1c695bdee08933229
Victor Falconi <v.falconi.m@gmail.com>
2024-11-27 09:16:07 -0500

    Rdtilod boton modal

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -64,7 +64,11 @@ const Search = ({isSearch, setIsSearch}) => {
                             title: '¡Búsqueda incompleta!',
                             text: 'Por favor, complete todos los campos.',
                             iconHtml: '<img src="busquedaIncompleta.png" style="width: 150px;"/>',
-                            customClass: styles.noBorder,
+                            customClass: {
+                              icon: styles.noBorder,
+                              confirmButton: styles.confirmButton,
+                            },
+                            buttonsStyling: false,
                           })
                         };
                     }}}/>
@@ -109,7 +113,11 @@ const Search = ({isSearch, setIsSearch}) => {
                         title: '¡Búsqueda incompleta!',
                         text: 'Por favor, complete todos los campos.',
                         iconHtml: '<img src="busquedaIncompleta.png" style="width: 150px;"/>',
-                        customClass: styles.noBorder,
+                        customClass: {
+                          icon: styles.noBorder,
+                          confirmButton: styles.confirmButton,
+                        },
+                        buttonsStyling: false,
                       })
                     };
                   }}>
