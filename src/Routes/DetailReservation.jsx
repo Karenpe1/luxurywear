2c85924c9f53e1972a5d2271b78860aa800fab87
tineojf <92743762+tineodev@users.noreply.github.com>
2024-12-03 14:14:42 -0500

    feat: add fetch function to receive detail reservation json

--- src/Routes/DetailReservation.jsx
+++ src/Routes/DetailReservation.jsx
@@ -15,6 +15,29 @@ function DetailReservation() {
   const [reservation, setReservation] = useState(null);
   const [mainImage, setMainImage] = useState("");
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    const fetchProductDetail = async () => {
+      try {
+        const responseReservation = await axios.get(
+          urlAPI + `/api/v1/reservations/${id}`
+        );
+        const dataReservation = await responseReservation.data;
+        setReservation(dataReservation);
+
+        const responseImages = await fetch(
+          urlAPI + `/api/v1/products/by-name/${dataReservation.productName}`
+        );
+        const data = await responseImages.json();
+        setProduct(data);
+        setMainImage(data.images[0]?.url ?? "placeholder.svg");
+      } catch (error) {
+        console.error("Error fetching product detail:", error);
+      }
+    };
+
+    fetchProductDetail();
+  }, [id, axios]);
 
   return (
     <div>
