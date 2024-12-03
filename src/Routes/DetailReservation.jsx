c8dff152d017d96aa3c698ba176c71b09f2c157c
tineojf <92743762+tineodev@users.noreply.github.com>
2024-12-03 14:16:07 -0500

    feat: add component to return function

--- src/Routes/DetailReservation.jsx
+++ src/Routes/DetailReservation.jsx
@@ -39,9 +39,135 @@ function DetailReservation() {
     fetchProductDetail();
   }, [id, axios]);
 
+  if (!product) return <p>Cargando...</p>;
+
   return (
-    <div>
-      Detalle de orden
+    <div className={styleReservation.detailContainer}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>{product.name}</h1>
+        <Link to="/" className={styles.backButton}>
+          {" "}
+          <img src={backButton} alt="Back" />
+        </Link>
+      </header>
+
+      <div className={styles.content}>
+        {/* Imagen principal */}
+        <img
+          src={`http://localhost:8080${mainImage}`}
+          alt={product.name}
+          className={styles.productImage}
+          onError={(e) => {
+            const fallback1 = `http://localhost:8080/${mainImage}`;
+            const fallback2 = `http://localhost:8080/public${mainImage}`;
+            const fallback3 = "placeholder.svg";
+            if (e.target.src === `http://localhost:8080${mainImage}`) {
+              e.target.src = fallback1;
+            } else if (e.target.src === fallback1) {
+              e.target.src = fallback2;
+            } else if (e.target.src === fallback2) {
+              e.target.src = fallback3;
+            } else {
+              e.target.onerror = null;
+            }
+          }}
+        />
+        <div className={styles.gallery}>
+          {/* Galería de miniaturas */}
+          {product.images.map((img, index) => (
+            <img
+              key={index}
+              src={`http://localhost:8080${img.url}`}
+              alt={`${product.name} thumbnail ${index + 1}`}
+              className={styles.thumbnail}
+              onClick={() => setMainImage(img.url)}
+              onError={(e) => {
+                const fallback1 = `http://localhost:8080/${img.url}`;
+                const fallback2 = `http://localhost:8080/public${img.url}`;
+                const fallback3 = "placeholder.svg";
+                if (e.target.src === `http://localhost:8080${img.url}`) {
+                  e.target.src = fallback1;
+                } else if (e.target.src === fallback1) {
+                  e.target.src = fallback2;
+                } else if (e.target.src === fallback2) {
+                  e.target.src = fallback3;
+                } else {
+                  e.target.onerror = null;
+                }
+              }}
+            />
+          ))}
+        </div>
+
+        <div className={styleReservation.titleReservation}>
+          Detalles de la reserva:
+        </div>
+
+        <div className={styleReservation.container}>
+          <div className={styleReservation.titleHidden}>
+            Detalles de tu reserva:
+          </div>
+          <div className={styleReservation.row}>
+            <span className={styleReservation.label}>Código de Reserva:</span>
+            <span className={styleReservation.value}>
+              {reservation.reservationCode}
+            </span>
+          </div>
+          <div className={styleReservation.row}>
+            <span className={styleReservation.label}>Nombre del producto</span>
+            <span className={styleReservation.value}>
+              {reservation.productName}
+            </span>
+          </div>
+          <div className={styleReservation.row}>
+            <span className={styleReservation.label}>Fecha de entrega:</span>
+            <span className={styleReservation.value}>
+              {reservation.startDate}
+            </span>
+          </div>
+          <div className={styleReservation.row}>
+            <span className={styleReservation.label}>Fecha de devolución:</span>
+            <span className={styleReservation.value}>
+              {reservation.endDate}
+            </span>
+          </div>
+          <div className={styleReservation.row}>
+            <span className={styleReservation.label}>Metodo de Envío:</span>
+            <span className={styleReservation.value}>
+              {reservation.shippingMethod}
+            </span>
+          </div>
+          <div className={styleReservation.row}>
+            <span className={styleReservation.label}>País:</span>
+            <span className={styleReservation.value}>
+              {reservation.country}
+            </span>
+          </div>
+          <div className={styleReservation.row}>
+            <span className={styleReservation.label}>Provincia:</span>
+            <span className={styleReservation.value}>
+              {reservation.province}
+            </span>
+          </div>
+          <div className={styleReservation.row}>
+            <span className={styleReservation.label}>Dirección:</span>
+            <span className={styleReservation.value}>
+              {reservation.address}
+            </span>
+          </div>
+          <div className={styleReservation.row}>
+            <span className={styleReservation.label}>Detalle:</span>
+            <span className={styleReservation.value}>{reservation.detail}</span>
+          </div>
+          <div className={styleReservation.row}>
+            <span className={styleReservation.label}>Costo Total:</span>
+            <span className={styleReservation.value}>
+              {formatCurrency(reservation.totalCost, "es-CO", "COP")}
+            </span>
+          </div>
+        </div>
+      </div>
+      <ModalGlobal />
     </div>
   );
 }
