719ea5c9dbbfbc87789258c146274fb498afe411
DavidBlanco2825 <davidfernando2825@hotmail.com>
2024-12-08 14:36:44 -0500

    chore: removed unused product images

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Replace the placeholder in the NGINX config with the actual API_BASE_URL value
-sed -i "s|\$API_BASE_URL|${API_BASE_URL}|g" /etc/nginx/conf.d/default.conf
-
-# Start NGINX
-nginx -g "daemon off;"
